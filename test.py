b08e0355fda314ae0952b58f34d52361793a3dac
Thrasi <thrasi@server.fake>
2013-08-21 22:25:04 +0200

    added a test for rotations

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ class TestMain(unittest.TestCase):
     def makeTree(self):
         self.tree=BST.BSTunbalanced(7)
         root = self.tree.root
-        self.tree.insert(root,4)
-        self.tree.insert(root,1)
-        self.tree.insert(root,5)
-        self.tree.insert(root,8)
-        self.tree.insert(root,7.5)
+        self.tree.insert(4)
+        self.tree.insert(1)
+        self.tree.insert(5)
+        self.tree.insert(8)
+        self.tree.insert(7.5)
         
     def testFind(self):
         self.makeTree()
@@ -73,6 +73,21 @@ class TestMain(unittest.TestCase):
         
         expected = 7
         result = self.tree.prior(7.5)
+        self.assertEqual(expected, result)
+        
+    def testRotation(self):
+        self.makeTree()
+        self.tree.insert(6)
+        expected = [[1, -1, -1, 4],[4, 1, -1, 5],[5, 4, 6, 7],[6, -1, -1, 5],
+                    [7, 5, 8, -1],[7.5, -1, -1, 8],[8, 7.5, -1, 7]]
+        self.tree.rotateLeft(4)
+        result = self.tree.relations(self.tree.root)
+        self.assertEqual(expected, result)
+        
+        expected = [[1, -1, -1, 4],[4, 1, 7, -1],[5, -1, 6, 7],[6, -1, -1, 5],
+                    [7, 5, 8, 4],[7.5, -1, -1, 8],[8, 7.5, -1, 7]]
+        self.tree.rotateRight(7)
+        result = self.tree.relations(self.tree.root)
         self.assertEqual(expected, result)
         
 if __name__ == '__main__':
